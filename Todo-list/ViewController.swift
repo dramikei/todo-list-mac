84e612cb01af4c9be39cbf80efab45c7b3a0d59d
Raghav Vashisht <raghav2011rv@gmail.com>
2018-07-23 12:20:25 +0530

    Initial Commit

---
+++ Todo-list/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Todo-list
+//
+//  Created by Raghav Vashisht on 23/07/18.
+//  Copyright © 2018 Raghav Vashisht. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
