3dcd9b6a01936adc7092f61f786bab310098d7d6
Raghav Vashisht <raghav2011rv@gmail.com>
2018-07-23 12:56:13 +0530

    wrote code to save data to CoreData

--- Todo-list/ViewController.swift
+++ Todo-list/ViewController.swift
@@ -9,7 +9,9 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var textField: NSTextField!
+    @IBOutlet weak var checkBox: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +23,27 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
+    @IBAction func addClicked(_ sender: NSButton) {
+        if textField.stringValue != "" {
+            let appDelegate = (NSApplication.shared.delegate as? AppDelegate)
+            if let context = appDelegate?.persistentContainer.viewContext {
+                let todoItem = TodoItem(context: context)
+                todoItem.name = textField.stringValue
+                if checkBox.state == .off {
+                    todoItem.important = false
+                    print("Not important")
+                } else if checkBox.state == .on {
+                    todoItem.important = true
+                    print("important")
+                }
+                appDelegate?.saveAction(nil)
+                textField.stringValue = ""
+                checkBox.state = .off
+                
+            }
+        }
+    }
+    
 
 }
 
